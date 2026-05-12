a2f863e0e3c91bd2ffdc622ffc6ef57f7f4c7e67
Goodluck <182907427+Goodluck-Kalu@users.noreply.github.com>
2026-05-12 19:35:05 +0100

    second commit first was an error

--- rocky.js
+++ rocky.js
@@ -1,5 +1,49 @@
 function getComputerChoice(){
-    let choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
-    
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
+function getHumanChoice(){
+    let humanChoice = prompt("Enter rock, paper, or scissors:");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
+let humanScore =0;
+let computerScore =0;
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+       
+        console.log("It's a tie!");
+    } else if (
+
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+    }
+        console.log("Current Score - You: " + humanScore + " Computer: " + computerScore);
+
+
+
+
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
